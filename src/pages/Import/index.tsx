968acbb665d4947be04da4f3aaa5500138397ba9
abauruel <abauruel@gmail.com>
2020-04-22 20:36:34 -0300

    added styles and functions

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import filesize from 'filesize';
-
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
 import Upload from '../../components/Upload';
@@ -23,19 +21,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file);
 
     try {
-      // await api.post('/transactions/import', data);
+      const response = await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
     // TODO
+    const arrayFiles: FileProps[] = [];
+    files.map(file => {
+      arrayFiles.push({
+        file,
+        name: file.name,
+        readableSize: file.size.toString(),
+      });
+      return file;
+    });
+    setUploadedFiles(arrayFiles);
   }
 
   return (
